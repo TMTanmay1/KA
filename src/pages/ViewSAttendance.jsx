59ff5b2c685710a27c7c04f8e6855d323bc9aa68
TMTanmay1 <tanmaymeshram883@gmail.com>
2024-10-04 04:34:58 +0530

    cursor_1

--- src/pages/ViewSAttendance.jsx
+++ src/pages/ViewSAttendance.jsx
@@ -245,7 +245,7 @@ const ViewSAttendance = () => {
                     variant="contained"
                     style={{
                       backgroundColor: 'red',
-                      cursor: !course.login_true ? 'pointer' : 'not-allowed',
+                      cursor: !course.login_true ? 'not-allowed' : 'pointer',
                       color: 'white',
                       fontWeight: 'bold',
                       borderRadius: '5px',
