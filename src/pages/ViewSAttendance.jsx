ff0db388c3cfbcb8f32e64d4ea415c5b3c0e726c
TMTanmay1 <tanmaymeshram883@gmail.com>
2024-10-04 04:36:24 +0530

    roll back

--- src/pages/ViewSAttendance.jsx
+++ src/pages/ViewSAttendance.jsx
@@ -245,7 +245,6 @@ const ViewSAttendance = () => {
                     variant="contained"
                     style={{
                       backgroundColor: 'red',
-                      cursor: !course.login_true ? 'not-allowed' : 'pointer',
                       color: 'white',
                       fontWeight: 'bold',
                       borderRadius: '5px',
